bd81818be5653da56a7f4cad2e1cd8c2a8e1fa64
Nora Liu <noraliu0830@gmail.com>
2023-11-20 15:15:57 +0800

    feat: add curriculum / outcome / FAQ sections

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -13,9 +13,14 @@ import {
   CardMedia,
   Link,
   Paper,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  TextField,
 } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const theme = createTheme({
   palette: {
@@ -92,14 +97,132 @@ const Footer = () => {
   );
 };
 
+const CurriculumSection = () => {
+  return (
+    <Container maxWidth="md">
+      <Box py={4}>
+        <Typography variant="h4" component="h2" gutterBottom>
+          Curriculum - The Path to Becoming a Web3 Engineer
+        </Typography>
+        <Grid container spacing={2}>
+          {/* Example card for part of the curriculum */}
+          <Grid item xs={12} sm={6} md={4}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  alt="contemplative reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Participate in Open Source Projects
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Get hands-on experience by contributing to real-world
+                    software.
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          {/* You can add more cards for different parts of the curriculum */}
+        </Grid>
+      </Box>
+    </Container>
+  );
+};
+
+const OutcomesSection = () => {
+  return (
+    <Container maxWidth="md">
+      <Box py={4}>
+        <Typography variant="h4" component="h2" gutterBottom>
+          Outcomes - Your Future After the Bootcamp
+        </Typography>
+        <Typography variant="subtitle1" paragraph>
+          Learn what you'll be able to achieve after completing the bootcamp and
+          how it will propel your career forward.
+        </Typography>
+        {/* Additional content and imagery would go here */}
+      </Box>
+    </Container>
+  );
+};
+
+const FAQSection = () => {
+  return (
+    <Container maxWidth="md">
+      <Box py={4}>
+        <Typography variant="h4" component="h2" gutterBottom>
+          Frequently Asked Questions
+        </Typography>
+        <div>
+          {/* Example accordion for FAQ item */}
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>What is Web3?</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Web3 refers to the new iteration of the World Wide Web based on
+                blockchain technology, featuring decentralization and
+                token-based economics.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          {/* More accordions for additional FAQs */}
+        </div>
+      </Box>
+    </Container>
+  );
+};
+
+const GetStartedSection = () => {
+  return (
+    <Container maxWidth="md">
+      <Box py={4} textAlign="center">
+        <Typography variant="h4" component="h2" gutterBottom>
+          Get Started - Join the Bootcamp
+        </Typography>
+        <Typography variant="subtitle1" paragraph>
+          Ready to take the next step in your career? Join our Web3 Frontend
+          Engineering Bootcamp today.
+        </Typography>
+
+        <form noValidate autoComplete="off">
+          <Box display="flex" justifyContent="center" mt={2}>
+            <TextField label="Email Address" variant="outlined" />
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              style={{ marginLeft: 16 }}
+            >
+              Get Started
+            </Button>
+          </Box>
+        </form>
+      </Box>
+    </Container>
+  );
+};
+
 const LandingPage = () => {
   return (
     <ThemeProvider theme={theme}>
       <Header />
       <main>
-        {/* Intro Section */}
         <IntroSection />
-        {/* Other sections would be here... */}
+        <CurriculumSection />
+        <OutcomesSection />
+        <FAQSection />
+        <GetStartedSection />
       </main>
       <Footer />
     </ThemeProvider>
