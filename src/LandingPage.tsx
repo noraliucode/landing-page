5974a2199a0421d9d458a58c1cc489be7fa6166f
Nora Liu <noraliu0830@gmail.com>
2023-11-21 17:23:45 +0800

    feat: add EventSection, Hackathon sections and adjust intro

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -17,10 +17,13 @@ import {
   AccordionDetails,
   AccordionSummary,
   TextField,
+  CardActions,
 } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import EventIcon from "@mui/icons-material/Event";
 
 const theme = createTheme({
   palette: {
@@ -44,7 +47,7 @@ const Header = () => {
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Nomad Engineer
+          Web3 Frontend
         </Typography>
         <Button color="inherit">Intro</Button>
         <Button color="inherit">Curriculum</Button>
@@ -58,24 +61,39 @@ const Header = () => {
 
 const IntroSection = () => {
   return (
-    <Container maxWidth="md">
-      <Box py={4}>
-        <Typography variant="h4" component="h2" gutterBottom>
-          Open Your Remote Life
-        </Typography>
-        <Typography variant="subtitle1" paragraph>
-          Study online, with flexible payment options and the full support of
-          industry pros and career mentors.
-        </Typography>
-        <Button
-          variant="contained"
-          color="primary"
-          endIcon={<ArrowForwardIcon />}
-        >
-          Learn More
-        </Button>
-      </Box>
-    </Container>
+    <Box
+      style={{
+        backgroundImage: "url(/assets/alvados.jpg)",
+        backgroundSize: "cover",
+        color: "black", // Adjust text color for readability
+        height: "65vh",
+      }}
+    >
+      <Container maxWidth="md">
+        <Box py={4}>
+          {" "}
+          {/* Optional overlay */}
+          <Container maxWidth="md">
+            <Box py={4}>
+              <Typography variant="h4" component="h2" gutterBottom>
+                Start Your Remote Life
+              </Typography>
+              <Typography variant="subtitle1" paragraph>
+                Study online, with flexible payment options and the full support
+                of industry pros and career mentors.
+              </Typography>
+              <Button
+                variant="contained"
+                color="primary"
+                endIcon={<ArrowForwardIcon />}
+              >
+                Learn More
+              </Button>
+            </Box>
+          </Container>
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
@@ -213,6 +231,110 @@ const GetStartedSection = () => {
   );
 };
 
+const EventSection = () => {
+  return (
+    <Container maxWidth="md">
+      <Box py={4}>
+        <Typography variant="h4" component="h2" gutterBottom>
+          Web3 Events
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image="/static/images/events/web3-event-1.jpg" // Replace with actual image paths
+                  alt="Web3 Event"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Global Web3 Conference
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Join the leading minds in the Web3 space to discuss the
+                    future of the internet.
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    size="small"
+                    color="primary"
+                    startIcon={<EventIcon />}
+                  >
+                    Learn More
+                  </Button>
+                  <Button
+                    size="small"
+                    color="primary"
+                    startIcon={<LocationOnIcon />}
+                  >
+                    Location
+                  </Button>
+                </CardActions>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          {/* Repeat for other events */}
+        </Grid>
+      </Box>
+    </Container>
+  );
+};
+
+const HackathonSection = () => {
+  return (
+    <Container maxWidth="md">
+      <Box py={4}>
+        <Typography variant="h4" component="h2" gutterBottom>
+          Hackathon in Foreign Countries
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image="/static/images/hackathons/hackathon-1.jpg" // Replace with actual image paths
+                  alt="Hackathon Event"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    International Hackathon
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Collaborate with international developers to build the next
+                    big Web3 innovation.
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    size="small"
+                    color="primary"
+                    startIcon={<EventIcon />}
+                  >
+                    Learn More
+                  </Button>
+                  <Button
+                    size="small"
+                    color="primary"
+                    startIcon={<LocationOnIcon />}
+                  >
+                    Location
+                  </Button>
+                </CardActions>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          {/* Repeat for other hackathons */}
+        </Grid>
+      </Box>
+    </Container>
+  );
+};
+
 const LandingPage = () => {
   return (
     <ThemeProvider theme={theme}>
@@ -220,6 +342,8 @@ const LandingPage = () => {
       <main>
         <IntroSection />
         <CurriculumSection />
+        <EventSection />
+        <HackathonSection />
         <OutcomesSection />
         <FAQSection />
         <GetStartedSection />
